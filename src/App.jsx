a81a1adcb314d683e0f3d50852ce18c65efa4b28
mohammadamzad38 <hossainamzad38@gmail.com>
2025-05-17 19:06:26 +0600

    motamoti shesh

--- src/App.jsx
+++ src/App.jsx
@@ -60,13 +60,36 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [xIsNext, setXIsNext] = useState(true);
+  const [currentMove, setCurrentMove] = useState(0);
 
-  const currentSquare = history[history.length - 1];
+  const currentSquare = history[currentMove];
 
   function handlePlay(nextSquares) {
     setXIsNext(!xIsNext);
-    setHistory([...history, nextSquares]);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   }
+
+  function jumpTo(move) {
+    setCurrentMove(move);
+    setXIsNext(move % 2 === 0);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+
+    if (move > 0) {
+      description = `Go to the move # ${move}`;
+    } else {
+      description = `Go to start the game`;
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
   return (
     <div>
@@ -74,7 +97,7 @@ export default function Game() {
         <Board xIsNext={xIsNext} squares={currentSquare} onPlay={handlePlay} />
       </div>
       <div>
-        <ol></ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
